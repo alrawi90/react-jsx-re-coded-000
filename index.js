5ebdbfcb6ecc0035830904ccda0f9ca113f839bc
Ali Alrawi <ali.developer90@gmail.com>
2017-02-21 12:15:57 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much.";
+// In a file called `Tweet.js`
+const React = require('react');
+
+class Tweet extends React.Component {
+  render(
+      <div className="tweet">
+        <img src="http://twitter.com/some-avatar.png" className="tweet__avatar" />
+        <div className="tweet__body">
+            <p>We are writing this tweet in JSX. Holy moly!</p>
+        </div>
+      </div>
+    )
+
+}
+module.exports = Tweet;
+// In a file in the same directory
+//const Tweet = require('./foo');
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('main')
+);
---
+++ test/index-test.js
@@ -0,0 +1,8 @@
+
+let expect=require("expect")
+describe('You in React', function () {
+
+    it('no test', function () {
+      expect(3).toEqual(3);
+    });
+  });
